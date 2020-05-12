928804358584cc4d47a5ce0781baddb7d1aa5552
Dizziest <marcin.michalowski45@gmail.com>
2020-05-12 13:20:12 +0200

    Added CocktailSearchResponse model.

---
+++ lib/models/cocktail_search_response.dart
@@ -0,0 +1,29 @@
+library cocktail_search_response;
+
+import 'dart:convert';
+
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+
+part 'cocktail_search_response.g.dart';
+
+abstract class CocktailSearchResponse implements Built<CocktailSearchResponse, CocktailSearchResponseBuilder> {
+
+  BuiltList<CocktailItem> get drinks;
+
+  CocktailSearchResponse._();
+
+  factory CocktailSearchResponse([updates(CocktailSearchResponseBuilder b)]) = _$CocktailSearchResponse;
+
+  String toJson(){
+    return json.encode(serializers.serializeWith(CocktailSearchResponse.serializer, this));
+  }
+
+  static CocktailSearchResponse fromJson(String jsonString){
+    return serializers.deserializeWith(CocktailSearchResponse.serializer, json.decode(jsonString));
+  }
+
+  static Serializer<CocktailSearchResponse> get serializer => _$cocktailSearchResponseSerializer;
+
+}
